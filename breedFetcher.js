bc285850b626744bad81579d5b64a24578e5e1fe
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-14 10:05:58 -0500

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,23 @@
 const request = require('request');
-let query = process.argv.slice(2);
-query = query.toString();
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + query;
-request(url , (error, response, body) => {
-  if (error !== null) {
-    console.log('error:', error); // Print the error if one occurred
-  }
-  if (query === '') {
-    console.log('Please enter a breed');
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+
+let breedName = process.argv.slice(2);
+breedName = breedName.toString();
+let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+const fetchBreedDescription = function(breedName, callback) {
+  callback(url , (error, response, body) => {
+    if (error !== null) {
+      console.log('error:', error); // Print the error if one occurred
+    }
+    if (breedName === '') {
+      console.log('Please enter a breed');
+    } else {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    }
+  });
+};
+
+fetchBreedDescription(breedName, request);
+
+module.exports = { fetchBreedDescription };
